0ffe2557ac13688f48c90ddfd666497945524af4
Mohamad_KH <Mohamad_KH@DESKTOP-NO1QG64>
2022-10-20 17:38:43 +0300

    blazor2

--- BlazorServer/Services/IMaterialService.cs
+++ BlazorServer/Services/IMaterialService.cs
@@ -1,6 +1,13 @@
-﻿namespace BlazorServer.Services
+﻿using Domain.Material;
+
+namespace BlazorServer.Services
 {
     public interface IMaterialService
     {
+        Task<IEnumerable<Material>> GetAllMaterials();
+        Task<Material> GetMaterial(int id);
+        Task<Material> AddMaterial(Material material);
+        Task UpdateMaterial(Material material);
+        Task DeleteMaterial(int id);
     }
 }
--- BlazorServer/Services/MaterialService.cs
+++ BlazorServer/Services/MaterialService.cs
@@ -1,7 +1,46 @@
-﻿namespace BlazorServer.Services
+﻿using Domain.Material;
+
+namespace BlazorServer.Services
 {
     public class MaterialService : IMaterialService
     {
+        private readonly HttpClient _httpClient;
+        public MaterialService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<Material> AddMaterial(Material material)
+        {
+            throw new NotImplementedException();
+        }
+        public Task UpdateMaterial(Material material)
+        {
+            throw new NotImplementedException();
+        }
 
+
+        public async Task<IEnumerable<Material>> GetAllMaterials()
+        {
+            IEnumerable<Material> materials;
+            try
+            {
+                materials = await _httpClient.GetFromJsonAsync<IEnumerable<Material>>("api/Material");
+            }
+            catch (Exception exception)
+            {
+                var ss = exception.Message;
+                throw new Exception(exception.Message);
+            }
+            return materials;
+        }
+
+        public Task<Material> GetMaterial(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task DeleteMaterial(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
